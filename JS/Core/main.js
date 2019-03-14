e80e5a824566d084e75f413c16ccf458322f8757
ImLonellyDammit <personal.markks@hotmail.com>
2019-03-14 18:17:47 +0000

    Update

--- JS/Core/main.js
+++ JS/Core/main.js
@@ -1,3 +1,4 @@
+// H1>H3 e vice-versa - Resize da Página - Ajustes de Texto
 $(window).resize(function() {
     if($(window).width() <= 490 )
     {
@@ -10,5 +11,23 @@ $(window).resize(function() {
         $('.inicio-text').find('h3').replaceWith(function() {
             return '<h1>' + $(this).html() + '</h1>';
         });
+    }
+});
+
+// Scroll Menu - Smoother Animation
+
+$(window).scroll(() => {
+    let scroll = $(window).scrollTop();
+    if (scroll == 0)
+        $('.navbar-nav').animate({padding:"20px"},500,"swing");
+    else
+    {
+        if (scroll >= 200)
+        {
+            $('.navbar').css("background-image","linear-gradient(to bottom right,transparent,black)");
+            $('.navbar-nav').animate({padding:"10px"},500);
+        }
+        else
+            $('.navbar').css("background-image","none");
     }
 });
